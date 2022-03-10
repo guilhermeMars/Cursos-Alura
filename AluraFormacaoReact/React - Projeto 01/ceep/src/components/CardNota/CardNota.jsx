e95f142b3746c0fa3ed88f15683dcc8dd5160c49
Guilherme Martins <guilhermemspiandorin@gmail.com>
2022-03-10 10:48:58 -0300

    Aula sobre SVG
    SVG adicionado no projeto
    Sem funcionalidade, por enquanto

--- AluraFormacaoReact/React - Projeto 01/ceep/src/components/CardNota/CardNota.jsx
+++ AluraFormacaoReact/React - Projeto 01/ceep/src/components/CardNota/CardNota.jsx
@@ -2,6 +2,7 @@
 
 import React, { Component } from "react"; // imrc
 import "./style.css";
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg";
 
 class CardNota extends Component {
   // cc
@@ -10,6 +11,7 @@ class CardNota extends Component {
       <section className="card-nota"> {/*className é a mesma coisa que o class, porém adaptado para o jsx*/}
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <DeleteSVG/> {/*<img src={DeleteSVG} alt="Deletar Nota" />*/}
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
